c786a46bf2d36aea8a6645dc3732c121778db71b
Vadim Samarin <dezivert@gmail.com>
2021-08-25 18:12:09 +0300

    Separate get_config

--- cli.js
+++ cli.js
@@ -1,38 +1,6 @@
 #!/usr/bin/env node
 
-const { resolve } = require('path');
 const eaciestCli = require('./src');
+const get_config = require('./get-config');
 
-const [,, ...args] = process.argv;
-const DEFAULT_CONFIG_PATH = './eaciest.config';
-
-const get_config_file_name = () => {
-  const idx = args.findIndex(x => x.includes('--config'));
-  if (idx === -1) {
-    return DEFAULT_CONFIG_PATH;
-  }
-
-  const match = /^--config(=(.+))$?/.exec(args[idx]);
-  if (match && match[2]) {
-    return match[2];
-  }
-
-  return args[idx + 1];
-}
-
-const get_config = (configFileName = get_config_file_name()) => {
-  const configPath = resolve(process.cwd(), configFileName);
-  try {
-    return require(configPath);
-  } catch (err) {
-    return {};
-  }
-}
-
-const config = get_config();
-
-eaciestCli(config);
-
-module.exports = {
-  get_config
-};
+eaciestCli(get_config());
---
+++ get-config.js
@@ -0,0 +1,29 @@
+const { resolve } = require('path');
+
+const [,, ...args] = process.argv;
+const DEFAULT_CONFIG_PATH = './eaciest.config';
+
+const get_config_file_name = () => {
+  const idx = args.findIndex(x => x.includes('--config'));
+  if (idx === -1) {
+    return DEFAULT_CONFIG_PATH;
+  }
+
+  const match = /^--config(=(.+))$?/.exec(args[idx]);
+  if (match && match[2]) {
+    return match[2];
+  }
+
+  return args[idx + 1];
+}
+
+const get_config = (configFileName = get_config_file_name()) => {
+  const configPath = resolve(process.cwd(), configFileName);
+  try {
+    return require(configPath);
+  } catch (err) {
+    return {};
+  }
+}
+
+module.exports = get_config;
